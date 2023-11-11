1688bbd7cf516a666a5d476d265a548f8aa55176
jawad <jmn09@mail.aub.edu>
2023-11-11 19:22:39 +0200

    structs

--- server.cpp
+++ server.cpp
@@ -1,7 +1,22 @@
 #include<bits/stdc++.h>
+#include<chrono>
+#include<ctime>
 using namespace std;
 
+struct{
 
+    int hour,min,s,day,m;
+
+};
+
+
+struct msg{
+    
+    int from;
+    string s;
+    time_t sendingTime;
+
+};
 
 int main(){
 
