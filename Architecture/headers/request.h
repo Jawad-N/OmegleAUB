08ee9ae1ae43c6f8ef5894f5c04fd50b8c518f88
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2023-12-13 16:38:34 +0200

    Chages

--- Architecture/headers/request.h
+++ Architecture/headers/request.h
@@ -1,3 +1,30 @@
 #pragma once
 
 #include "includes.h"
+
+class request
+{
+private:
+    int request_id;
+    request_t req_type;
+    string from;
+
+public:
+    static int counter;
+
+    request();
+    request(request_t req_type);
+
+    int requestId() const { return request_id; }
+    void setRequestId(int requestId) { request_id = requestId; }
+
+    request_t reqType() const { return req_type; }
+    void setReqType(const request_t &reqType) { req_type = reqType; }
+
+    string getFrom() const { return from; }
+    void setFrom(const string &from_) { from = from_; }
+};
+
+class request_connect : public request
+{
+};
